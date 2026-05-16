b75e5ec80857b19a75a44dd811416f1f40edc760
AzatValiev <email@email.com>
2026-05-16 18:16:44 +0300

    Выполнить задание 6 спринта

---
+++ src/ru/yandex/practicum/gym/CounterOfTrainings.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.gym;
+
+public class CounterOfTrainings implements Comparable<CounterOfTrainings>{
+    Coach coach;
+    int count;
+
+    public CounterOfTrainings(Coach coach, int count) {
+        this.coach = coach;
+        this.count = count;
+    }
+
+@Override
+    public int compareTo(CounterOfTrainings o) {
+return Integer.compare(o.count, this.count);
+}
+
+}
--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,66 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
+    //TreeMap<TimeOfDay, List<TrainingSession>> currentDaySessions = new TreeMap<>();
+    //List<TrainingSession> currentTimeSessions = new ArrayList<>();
+
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
+
+        if (trainingSession.getTimeOfDay().getHours() < 0 || trainingSession.getTimeOfDay().getHours() > 23 ||
+                trainingSession.getTimeOfDay().getMinutes() < 0 || trainingSession.getTimeOfDay().getMinutes() > 59) {
+            System.out.println("Введено некорректное время!");
+            return;
+        }
+
+        if (timetable.containsKey(trainingSession.getDayOfWeek())) {
+            TreeMap<TimeOfDay, List<TrainingSession>> currentDaySessions = timetable.get(trainingSession.getDayOfWeek());
+            if (currentDaySessions.containsKey(trainingSession.getTimeOfDay())) {
+                List<TrainingSession> currentTimeSessions = currentDaySessions.get(trainingSession.getTimeOfDay());
+                currentTimeSessions.add(trainingSession);
+                currentDaySessions.put(trainingSession.getTimeOfDay(), currentTimeSessions);
+            } else {
+                currentDaySessions.put(trainingSession.getTimeOfDay(),
+                        new ArrayList<TrainingSession>(List.of(trainingSession)));
+                timetable.put(trainingSession.getDayOfWeek(), currentDaySessions);
+            }
+        } else {
+            TreeMap<TimeOfDay, List<TrainingSession>> currentDaySessions = new TreeMap<>();
+            currentDaySessions.put(trainingSession.getTimeOfDay(), new ArrayList<TrainingSession>(List.of(trainingSession)));
+            timetable.put(trainingSession.getDayOfWeek(), currentDaySessions);
+        }
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public TreeMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.get(dayOfWeek);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        return timetable.get(dayOfWeek).get(timeOfDay);
+    }
+
+    public List<CounterOfTrainings> getCountByCoaches() {
+        Map<Coach, Integer> coachMap = new HashMap<>();
+        if (timetable.isEmpty()) {
+            System.out.println("Занятий нет!");
+            return null;
+        }
+        for (Map.Entry<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> entry : timetable.entrySet()) {
+            Map<TimeOfDay, List<TrainingSession>> daySession = entry.getValue();
+            for (Map.Entry<TimeOfDay, List<TrainingSession>> insideEntry : daySession.entrySet()) {
+                for (TrainingSession trainingSession : insideEntry.getValue()) {
+                    int count = coachMap.getOrDefault(trainingSession.getCoach(),0);
+                    coachMap.put(trainingSession.getCoach(), count + 1);
+                }
+            }
+        }
+        List<CounterOfTrainings> counter = new ArrayList<>();
+        for (Map.Entry<Coach, Integer> entry : coachMap.entrySet()) {
+            counter.add(new CounterOfTrainings(entry.getKey(), entry.getValue()));
+        }
+        Collections.sort(counter);
+        return counter;
     }
 }
